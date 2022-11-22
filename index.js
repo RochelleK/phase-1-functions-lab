c451272b1c992714acd7ec194a341e6d5f56b242
Rochelle Katzman <zelrochelle@gmail.com>
2022-11-22 13:57:13 -0500

    commit tree 382f83c5b0118849651fc9a6ebf880038a4a8d45

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someVal){
+    if (someVal >42){
+        return someVal-42;
+    } else {
+        return 42-someVal; 
+    }
+}
+
+function distanceFromHqInFeet(someVal){
+    const x = distanceFromHqInBlocks(someVal);
+    return x*264;
+
+}
+
+function distanceTravelledInFeet(start, destination){
+    const x = destination - start; 
+    return Math.abs(x)*264; 
+}
+
+function calculatesFarePrice(start, destination){
+    const x = distanceTravelledInFeet(start, destination)
+    if (x<=400){
+        return 0; 
+    }
+
+    else if (x >400 && x <= 2000){
+     return (x-400) * .02;
+    }
+    else if (x > 2000 && x < 2500) {
+        return 25
+    } 
+    else if  (x > 2500){ 
+    return 'cannot travel that far'
+    }
+    /*
+    switch (x){
+    case x<=400:
+        return 0; 
+    break;  
+    case x >400 && x <= 2000: 
+     return (x-400) * .02;
+    break; 
+    case x > 2000 && x < 2500:
+        return 25; 
+        break; 
+    case x > 2500: 
+    return 'cannot travel that far'
+    break; 
+    }*/
+}
